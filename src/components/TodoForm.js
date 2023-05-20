e21107cf2757d7e3dae0695eb1d1112353df3751
kjthier <karinjthier@gmail.com>
2023-05-20 04:42:23 -0400

    offline merge - form, item, list components

---
+++ src/components/TodoForm.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+export default function TodoForm({onSubmit}) {
+
+    // Creates storage for input field string:
+    const [newItem, setNewItem] = useState('')
+
+    // Add a todo functionality:
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        // Check if newItem is empty or not first:
+        if (newItem === '') return
+
+        // Call the addTodo fcn to the form, passing in newItem:
+        onSubmit(newItem)
+
+        // Clears input field after 'Add' is clicked:
+        setNewItem('')
+    }
+    
+    return (
+        <form onSubmit={handleSubmit} className='new-item-form'>
+            <div className='form-row'>
+                <label htmlFor='item'>New Item</label>
+                <input 
+                    value={newItem} 
+                    onChange={e => setNewItem(e.target.value)}
+                    type='text' 
+                    id='item' 
+                />
+            </div>
+            <button className='btn'>Add</button>
+        </form>
+    )
+}
